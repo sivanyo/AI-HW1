1c1bada12a83d138aaf3ce02e0137c776c767a54
sivanyo <57237255+sivanyo@users.noreply.github.com>
2020-11-14 13:34:21 +0200

    try things
    try things

--- problems/map_problem.py
+++ problems/map_problem.py
@@ -3,7 +3,6 @@ from framework import *
 from typing import Iterator
 from dataclasses import dataclass
 
-
 __all__ = ['MapState', 'MapProblem']
 
 
@@ -40,7 +39,7 @@ class MapProblem(GraphProblem):
         self.streets_map = streets_map
         self.target_junction_id = target_junction_id
         self.name += f'(src: {source_junction_id} dst: {target_junction_id})'
-    
+
     def expand_state_with_costs(self, state_to_expand: GraphProblemState) -> Iterator[OperatorResult]:
         """
         For a given state, iterates over its successor states.
@@ -54,6 +53,10 @@ class MapProblem(GraphProblem):
         # Get the junction (in the map) that is represented by the state to expand.
         junction = self.streets_map[state_to_expand.junction_id]
 
+        for i in junction.outgoing_links:
+            if i is not None:
+                yield OperatorResult((MapState(i)), i.distance)
+
         # TODO [Ex.10]:
         #  Read the documentation of this method in the base class `GraphProblem.expand_state_with_costs()`.
         #  Finish the implementation of this method.
@@ -66,7 +69,8 @@ class MapProblem(GraphProblem):
         #  Note: Generally, in order to check whether a variable is set to None you should use the expression:
         #        `my_variable_to_check is None`, and particularly do NOT use comparison (==).
 
-        yield OperatorResult(successor_state=MapState(self.target_junction_id), operator_cost=7)  # TODO: remove this line!
+        # yield OperatorResult(successor_state=MapState(self.target_junction_id), operator_cost=7)  # TODO: remove
+        #  this line!
 
     def is_goal(self, state: GraphProblemState) -> bool:
         """
@@ -76,4 +80,4 @@ class MapProblem(GraphProblem):
 
         # TODO [Ex.10]: modify the returned value to indicate whether `state` is a final state.
         # You may use the problem's input parameters (stored as fields of this object by the constructor).
-        return state.junction_id == 14593  # TODO: modify this!
+        return state.junction_id == self.target_junction_id  # TODO: modify this!
