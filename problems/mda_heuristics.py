38f44a6eead00024b105147a91ec3390a62d6fc9
sivanyo <57237255+sivanyo@users.noreply.github.com>
2020-11-21 19:15:01 +0200

    fixed g cost func !!!!!

--- problems/mda_heuristics.py
+++ problems/mda_heuristics.py
@@ -193,8 +193,10 @@ class MDATestsTravelDistToNearestLabHeuristic(HeuristicFunction):
             return min(self.cached_air_distance_calculator.get_air_distance_between_junctions(junction, lab.location)
                        for lab in labs)
 
-        tmp = air_dist_to_closest_lab(state.current_site.location) * sum(tests.nr_roommates for tests in
-                                                                         state.tests_on_ambulance)
+        tmp = 0
+        if state.tests_on_ambulance is not frozenset():
+            tmp = air_dist_to_closest_lab(state.current_site.location) * sum(tests.nr_roommates for tests in
+                                                                             state.tests_on_ambulance)
 
         return tmp + sum(air_dist_to_closest_lab(app.location) * app.nr_roommates for app in
                          self.problem.get_reported_apartments_waiting_to_visit(state))
